32bac00daf6d0fe45ae374cba6c3ffc90a5a1503
Nikhil Anumandla <m1064495@mindtree.com>
2021-06-15 10:53:26 +0530

    Initial commit

---
+++ core/src/main/java/com/orchard/core/models/ApacheHttpModel.java
@@ -0,0 +1,43 @@
+package com.orchard.core.models;
+
+import javax.inject.Inject;
+
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.Default;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.OSGiService;
+
+import com.orchard.core.service.ApacheHttp;
+
+
+@Model(adaptables=Resource.class)
+public class ApacheHttpModel {
+	
+	@Inject
+	@Default(values="India")
+	String country;
+	String name ="no" ;
+	String capital="no";
+	String image="no";
+	
+	@OSGiService
+	ApacheHttp http;
+	
+	public String getData() {
+		return http.getData(country);
+	}
+	
+	public String getCountry() {
+		return country;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public String getCapital() {
+		return capital;
+	}
+	public String getImage() {
+		return image;
+	}
+}
---
+++ core/src/main/java/com/orchard/core/service/ApacheHttp.java
@@ -0,0 +1,4 @@
+package com.orchard.core.service;
+public interface ApacheHttp {
+	public String getData(String country);
+}
